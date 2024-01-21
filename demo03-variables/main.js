6d461c5904767ca2b732ac30ce546e9fc02a80ea
Uncle Lin <unclelin2020@gmail.com>
2024-01-21 14:59:47 +0800

    fix: variable demo

--- demo03-variables/main.js
+++ demo03-variables/main.js
@@ -1,6 +1,2 @@
 const input = document.querySelector("input");
 const password = document.querySelector("#password");
-
-input.addEventListener("input", function (e) {
-  password.innerHTML = e.target.value;
-});
